b0273502871d593bd08568ac20e83f99772c1a6d
Oleg <senyuholeg@gmail.com>
2017-05-31 18:36:19 +0300

    first commit

---
+++ src/first/Test.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Test {
+   private String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+    public void myName (String name){
+        System.out.println("My name is " + name);
+    }
+}
---
+++ src/second/Test.java
@@ -0,0 +1,12 @@
+package second;
+
+public class Test {
+    private int a;
+    private int b;
+
+    public Test(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+}
